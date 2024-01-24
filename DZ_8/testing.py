a345c93c0dddecbde8d061d95e250972f72b15cd
John Doe <johndoe@example.com>
2024-01-24 21:17:46 +0300

    DZ_8, first commit

---
+++ DZ_8/testing.py
@@ -0,0 +1,11 @@
+def multiplication_magic(a: int, b: int) -> int:
+    """Some multiplication func"""
+    result_mult: int = a * b
+    return result_mult
+
+
+var_1: int = 15
+var_2: int = 25
+
+result: int = multiplication_magic(var_1, var_2)
+print(result)
