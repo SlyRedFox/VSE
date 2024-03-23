fc74b49541997418400b517224a67dad1af36483
John Doe <johndoe@example.com>
2024-03-23 14:02:08 +0300

    afina_pallada is ready

--- Crypto/Practical_work_1/afina_pallada.py
+++ Crypto/Practical_work_1/afina_pallada.py
@@ -1,79 +1,87 @@
-# Афинный шифр, Python-воплощение
+# Афинный шифр
 # Шифр подстановочный. Криптографическое преобразование заменяет символы открытого текста на другие по правилу.
 from pprint import pprint
+from general_store import arabian_digits
+from general_store import checkin_input_word
+from general_store import true_alphabet
+from general_store import simple_exit
+from general_store import rae
 
 
-arabian_digits: list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
-rus_alphabet: list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-
-# TODO: del
-# digits_eng: list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+# TODO: del продумать, будут ли английские символы?
+digits_eng: list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 eng_alphabet: list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'i', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 eng_lettet_digit: dict = dict(zip(eng_alphabet, arabian_digits))
-# print(eng_lettet_digit)
 
 # словарь {буква: цифра}
-rus_lettet_digit: dict = dict(zip(rus_alphabet, arabian_digits))
+rus_lettet_digit: dict = dict(zip(true_alphabet, arabian_digits))
 # словарь {цифра: буква}
-rus_digit_letter: dict = dict(zip(arabian_digits, rus_alphabet))
-print(rus_lettet_digit)
+rus_digit_letter: dict = dict(zip(arabian_digits, true_alphabet))
 
 
-# TODO добавить input() введите слово для зашифровки Афинным шифром. Сейчас просто слово.
-word_encrypt: str = 'вфеврале'
+our_word: str = input('\nЗдравствуй, %username%! \nПожалуйста, введи слово для «Афинного шифра» (только кириллица). '
+                      'Спасибо! \nP.S.: есть базовая проверка ввода: ')
+
+checkin_input_word(our_word)
 
 # прописываем соответствие "буква алфавита - номер"
 print('\nПеревод элементов слова для шифрования в арабские цифры.')
 numbers_of_letters: list = []
-for letter in word_encrypt:
+for letter in our_word.lower():
     numbers_of_letters.append(rus_lettet_digit[letter])
-# 2, 21, 5, 2, 17, 0, 12, 5 (это х1, х2 ... хn)
 print(numbers_of_letters)
 
 
-# TODO: del Задаём ключ. Это пара чисел alpha и beta, которые принадлежат кольцу класса вычетов пункта "ключ" 7:00
-# длина rus алфавита, из этой длины random можно выбрать beta
-n: int = 33
+# длина русского алфавита, из этой длины random можно выбрать beta
+n_mod: int = 33
 
-# alpha может относиться только к группе обратимых эл-ов кольца класса вычетов.
-# Это эл-ты, которые взаимно простые с числом n, т. е. те, у которых НОД с n = 1
+# alpha может относиться только к группе обратимых элементов кольца класса вычетов.
+# Это элементыты, которые взаимно простые с числом n_mod, т. е. те, у которых НОД с n_mod = 1
 # Число 7 взаимно простое с 33, т.к. у них разные простые множители, и они не имеют общих делителей, кроме 1.
 # Число 33, делители: 1, 3, 11, 33
 # Число 7, делители: 1, 7.
 # Ни один из делителей числа 33 не является делителем числа 7. Это означает, что число 7 и число 33 взаимно простые.
-# TODO del - подбор можно сделать через 4_rae.py, если в Checking 1, подходит
 alpha: int = 7
-beta: int = 14 # random число от 0 до 32, нашего n
+beta: int = 14 # random число от 0 до 32, нашего n_mod
 
-# находим список y-ов по ф-ле из раздела "Зашифрование" уi = (alpha * xi + beta) mod n
+# находим список y-ов по ф-ле из раздела "Зашифрование" уi = (alpha * xi + beta) mod n_mod
 # x1 = (7 * 2 + 14) % 33
 crypto_symbols: list = list()
 for digit in numbers_of_letters:
-    temp_y = (alpha * digit + beta) % n
+    temp_y = (alpha * digit + beta) % n_mod
     crypto_symbols.append(rus_digit_letter[temp_y])
 
 print('\nЗашифрованная фраза:')
 pprint(crypto_symbols)
 
-print('\nОсуществляем проверку. Воспользуемся сначала Расширенным Алгоритмом Евклида для нахождения обратного эл-та.')
-# TODO: внедрить 4_rae.py результат: 7**-1 mod 33 = 19
 
-print('\nПроверяем корректность найденного обратного эл-та.')
-# Ф-ла: (alpha * alpha**-1) ≡ 1 * mod n
-# В нашем случае: 7 * 19 ≡ 1 * mod 33 = 133 ≡ 1 * mod 33 = 133 mod 33 = 1
+print('\nОсуществляем проверку. С помощью Расширенного алгоритма Евклида найдём обратный элемент.')
+reverse_element: int = rae(alpha, n_mod)
+print(f'Результат вычисления РАЕ: {reverse_element}')
 
-# Проверяем по ф-ле из раздела "Расшифрование" xi = alpha**-1 * (yi - beta) mod n = 19 * ()
+
+print('\nПроверяем корректность найденного обратного элемента.')
+# Ф-ла: (alpha * alpha**-1) ≡ 1 * mod n_mod
+# В базовом случае: 7 * 19 ≡ 1 * mod 33 = 133 ≡ 1 * mod 33 = 133 mod 33 = 1
+checking_reverse_element: int = (alpha * reverse_element) % n_mod
+if checking_reverse_element == 1:
+    print('Проверка пройдена успешно!')
+else:
+    print('Проверка не пройдена! Завершаем программу...')
+    simple_exit()
+
+# Проверяем по ф-ле из раздела "Расшифрование" xi = alpha**-1 * (yi - beta) mod n_mod
 print('\nПроверяем элементы зашифрованной фразы.')
 elements_check: list = list()
 for elem in crypto_symbols:
-    temp_x = 19 * (rus_lettet_digit[elem] - beta) % n
+    temp_x = reverse_element * (rus_lettet_digit[elem] - beta) % n_mod
     elements_check.append(temp_x)
 
 print(elements_check)
 
 # Финал-проверка
 final_check: list = list()
-print('\nВосстанавливаем исходную фразу.')
+print('\nВосстанавливаем исходное слово-фразу:')
 for elem in elements_check:
     final_check.append(rus_digit_letter[elem])
 
@@ -81,5 +89,10 @@ our_first_word: str = ''
 for elem in final_check:
     our_first_word += elem
 
-# TODO: посмотреть описание Афинного шифра в задании
 print(our_first_word)
+if our_first_word == our_word:
+    print('Исходное слово-фраза и расшифровоанное совпадают!')
+else:
+    print('Расшифрованная слово-фраза не совпадает с исходной!')
+
+# TODO: посмотреть описание Афинного шифра в задании
--- Crypto/Practical_work_1/simple_change.py
+++ Crypto/Practical_work_1/simple_change.py
@@ -1,44 +1,19 @@
 # Шифр простой замены
-from time import sleep
+from general_store import true_alphabet
+from general_store import crypto_alphabet
+from general_store import checkin_input_word
 
 
-true_alphabet: list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
-                       'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-crypto_alphabet: list = ['б', 'а', 'г', 'в', 'е', 'д', 'ж', 'ё', 'и', 'з', 'к', 'й', 'м', 'л', 'о', 'н', 'р', 'п', 'т',
-                         'с', 'ф', 'у', 'ц', 'х', 'ш', 'ч', 'ъ', 'щ', 'ь', 'ы', 'я', 'э', 'ю']
-
-# для проверки корректного ввода, английский алфавит и спецсимволы
-eng_alphabet_spec: list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'i',
-                           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(',
-                           ')', '_', '+', '-', '"', '№', ';', ':', '?', '<', '>', ',' '.', '/', '\\', '|', '', '~',
-                           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                           'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-# оздаём два словаря для зашифровки и расшифровки
+# создаём два словаря для зашифровки и расшифровки
 true_crypto_lettsrs: dict = dict(zip(true_alphabet, crypto_alphabet))
 crypto_true_letters: dict = dict(zip(crypto_alphabet, true_alphabet))
 
-
-# ф-ия выхода
-def simple_exit():
-    print('\nВведены предосудительные символы!\nПожалуйста, запустите программу заново и выполните корректный ввод!')
-    exit()
-
-
-our_word: str = input('Здравствуй, %username%! \nПожалуйста, введи слово, которое состоит из русских (кириллица) букв! '
+our_word: str = input('Здравствуй, %username%! \nПожалуйста, введи слово для «Шифра простой замены» (только кириллица).'
                       'Спасибо! \nP.S.: есть базовая проверка ввода: ')
 
-print('\nПроверочная фаза, пожалуйста, подождите...')
-sleep(1)
-if len(our_word) == 0:
-    simple_exit()
+checkin_input_word(our_word)
 
-for key in our_word:
-    print(f'\nТщательно проверяем символ: {key}')
-    if (len(key) == 0) or (key in eng_alphabet_spec):
-        simple_exit()
-
-print(f'\nАктивируем шифр простой замены, результат для слова «{our_word}»:')
+print(f'\nАктивируем «Шифр простой замены», результат для слова «{our_word}»:')
 crypto_word: str = ''
 for letter in our_word.lower():
     crypto_word += true_crypto_lettsrs[letter]
